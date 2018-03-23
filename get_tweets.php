3efb8681902f1ef5fb382b658eb053b041b1957e
allisonlcching <allisonlcccc@gmail.com>
2018-03-22 20:10:46 -0600

    added 3 id, fullText

--- get_tweets.php
+++ get_tweets.php
@@ -9,8 +9,12 @@
 	);
 
 
-	$url = 'https://api.twitter.com/1.1/statuses/show.json';
-	$getfield = '?id=975892164533108736';
+// 976557844022616068 = size tweet1
+// 954489156654247936 = burger tweet2
+// 976816837529923584 = recovery tweet3
+	
+	$url = 'https://api.twitter.com/1.1/statuses/lookup.json';
+	$getfield = '?id=976557844022616068,954489156654247936,976816837529923584&tweet_mode=extended';
 	$requestMethod = 'GET';
 
 	$twitter = new TwitterAPIExchange($settings);
--- js/main.js
+++ js/main.js
@@ -24,20 +24,34 @@ function init() {
 		$('.resource03').removeClass('hide');
 		$('.resource02').removeClass('hide');
   	});
-
-	$('.Allure').mouseover(function() {
-		$('.resource-text02').addClass('open');
-		$('.resource03').addClass('hide');
+  	$('.Allure').mouseover(function() {
+  		$('.resource-text03').addClass('open');
 		$('.resource01').addClass('hide');
-	});
-	$('.Allure').mouseout(function() {
-    	$('.resource-text02').removeClass('open');
-		$('.resource03').removeClass('hide');
-		$('.resource01').removeClass('hide');
+		$('.resource02').addClass('hide');
   	});
+	$('.Allure').mouseout(function() {
+    	$('.resource-text03').removeClass('open');
+		$('.resource01').removeClass('hide');
+		$('.resource02').removeClass('hide');
+  	});
+
+  	var think01 = $('.think01');
+	var showTweet1 = $('.showTweet1');
 
 	var think02 = $('.think02');
 	var showTweet2 = $('.showTweet2');
+
+	var think03 = $('.think03');
+	var showTweet3 = $('.showTweet3');
+
+	think01.click(function() {
+		$(this).hide();
+		showTweet1.show();
+	});
+	showTweet1.click(function() {
+		$(this).hide();
+		think01.show();
+	});
 
 	think02.click(function() {
 		$(this).hide();
@@ -48,7 +62,19 @@ function init() {
 		think02.show();
 	});
 
+	think03.click(function() {
+		$(this).hide();
+		showTweet3.show();
+	});
+	showTweet3.click(function() {
+		$(this).hide();
+		think03.show();
+	});
+
+	var tweet1 = document.getElementById("tweet1");
 	var tweet2 = document.getElementById("tweet2");
+	var tweet3 = document.getElementById("tweet3");
+
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'get_tweets.php', true);
 	xhr.send(null);
@@ -56,7 +82,10 @@ function init() {
 		if(xhr.status == 200) {
 			var tweets = JSON.parse(xhr.responseText);
 
-			tweet2.innerHTML = "&quot;" + tweets.text + "&quot;";
+			tweet1.innerHTML = "&quot;" + tweets[0].full_text + "&quot;";
+			// tweet2.innerHTML = "&quot;" + tweets.statuses[0].created_at + "&quot;";
+			tweet2.innerHTML = "&quot;" + tweets[1].full_text + "&quot;";
+			tweet3.innerHTML = "&quot;" + tweets[2].full_text + "&quot;";
 
 			// tweet2.innerHTML = "<img src=" + tweets.user.profile_image_url_https + ">";
 
