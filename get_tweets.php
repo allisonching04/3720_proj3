9e88e68ebf7928ea61ad10c65871c128f40365e2
allisonlcching <allisonlcccc@gmail.com>
2018-03-24 16:35:52 -0600

    project 3 done

--- get_tweets.php
+++ get_tweets.php
@@ -2,10 +2,10 @@
 	require_once('TwitterAPIExchange.php');
 
 	$settings = array(
-	    'oauth_access_token' => "957769973627146240-zXdN20gAc72csJ9qD1Muc3oxPI3Eitz",
-	    'oauth_access_token_secret' => "yZpbOY8XqwOTzt8GoTUinFPU1ALFskV4JKGw8V2mK9BN3",
-	    'consumer_key' => "lqhsIJttV2JNn6kF3wvKiAofI",
-	    'consumer_secret' => "qPVTVE7j98cNtmaCn3k7owRVJN7TDpzYW5SedTpc22VSMkirZK"
+	    'oauth_access_token' => "#",
+	    'oauth_access_token_secret' => "#",
+	    'consumer_key' => "#",
+	    'consumer_secret' => "#"
 	);
 
 
